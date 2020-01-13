fcb117e726720240fcf045e100498d14ac694b1d
Meow Hung <meowhung@admins-Air.net.fpt>
2020-01-13 16:50:28 +0700

    add shop page

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,32 +12,32 @@ class Directory extends React.Component {
             sections: [
                 {
                     title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    imageUrl: 'https://shop.r10s.jp/rockingchair/cabinet/04506184/imgrc0068634247.jpg',
                     id: 1,
                     linkUrl: 'shop/hats'
                 },
                 {
                     title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    imageUrl: 'https://img-static.tradesy.com/item/24610699/zara-olive-green-faux-fur-lines-parka-winter-coat-size-6-s-7-0-960-960.jpg',
                     id: 2,
                     linkUrl: 'shop/jackets'
                 },
                 {
                     title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    imageUrl: 'https://i.pinimg.com/originals/ea/6a/d5/ea6ad5dc64fb98eab3f9156662fbedec.jpg',
                     id: 3,
                     linkUrl: 'shop/sneakers'
                 },
                 {
                     title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    imageUrl: 'https://scontent-lhr3-1.cdninstagram.com/v/t51.2885-15/e35/25022079_1917788338538356_8065178185014181888_n.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com&_nc_cat=104&se=7&oh=25d5edb8075f0ef224ebc93aad911780&oe=5E227D9B&ig_cache_key=MTY2NzEwMzgyNDI0OTUxMDcyOQ%3D%3D.2',
                     size: 'large',
                     id: 4,
                     linkUrl: 'shop/womens'
                 },
                 {
                     title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    imageUrl: 'https://media2.prime.md/image/201711/1140x/barbati_15316300.jpg',
                     size: 'large',
                     id: 5,
                     linkUrl: 'shop/mens'
